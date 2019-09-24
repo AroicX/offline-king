b5397bc069df767a2e697e213858b80dea6e116b
AroicX <Arowosegbe67@gmail.com>
2019-09-24 11:44:16 +0100

    ....

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -5,8 +5,11 @@
   $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
+    $("#mini-logo").toggleClass("active-mini");
     if ($(".sidebar").hasClass("toggled")) {
       $('.sidebar .collapse').collapse('hide');
+
+    
     };
   });
 
